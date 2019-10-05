a826596dd08c6a16bc90ac522e5343f2742496c4
Ken SONODA <ksoda@users.noreply.github.com>
2019-10-06 02:03:21 +0900

    Draft extension

--- app/scripts/contentscript.ts
+++ app/scripts/contentscript.ts
@@ -2,3 +2,38 @@
 // import 'chromereload/devonly'
 
 console.log(`'Allo 'Allo! Content script`);
+
+chrome.runtime.onMessage.addListener(m => {
+  console.log(m);
+  if (m.type !== "search") return;
+  const d = new Date(
+    Date.parse(
+      document
+        .querySelector(
+          "[role=article] > div > div > div > span:nth-child(1) > span"
+        )!
+        .textContent!.split("·")[1]
+    )
+  );
+  location.href = `/search?q=${encodeURI(
+    toTwQuery({
+      from: window.location.pathname.split("/")[1],
+      since: toDateStr(delta(d, -1)),
+      until: toDateStr(delta(d, 1))
+    })
+  )}`;
+});
+
+function toDateStr(d: Date): string {
+  return d.toISOString().split("T")[0];
+}
+function delta(d: Date, x: number): Date {
+  const d_ = new Date(d.getTime());
+  d_.setDate(d_.getDate() + x);
+  return d_;
+}
+function toTwQuery(r: {}): string {
+  return Object.entries(r)
+    .map(x => x.join(":"))
+    .join(" ");
+}
--- app/scripts/popup.ts
+++ app/scripts/popup.ts
@@ -2,3 +2,11 @@
 // import 'chromereload/devonly'
 
 console.log(`'Allo 'Allo! Popup`);
+
+document.querySelector("button")!.addEventListener("click", e => {
+  console.log(e);
+  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+    console.log(tabs);
+    chrome.tabs.sendMessage(tabs[0].id!, { type: "search" });
+  });
+});
